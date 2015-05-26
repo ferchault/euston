b8305e59d21e2d967de7dbe0b25ce1d485b2f523
Guido Falk von Rudorff <guido@vonrudorff.de>
2015-05-26 15:12:28 +0100

    fixed paths issue

--- tests/test_cp2kinput.py
+++ tests/test_cp2kinput.py
@@ -1,5 +1,6 @@
 import unittest
 import numpy as np
+import os
 
 from euston.io import Cp2kInput
 import StringIO
@@ -15,11 +16,14 @@ SNAFU 2
 &END SECTIONA'''
 
 class TestCp2kInput(unittest.TestCase):
+    def _get_data_file(self, filename):
+        return os.path.join(os.path.dirname(os.path.realpath(__file__)), 'data', filename)
+
     def test_load(self):
-        cp2k = Cp2kInput('./data/regtest-ot-1-Ar-14.inp')
+        cp2k = Cp2kInput(self._get_data_file('regtest-ot-1-Ar-14.inp'))
 
     def test_boolean(self):
-        cp2k = Cp2kInput('./data/regtest-ot-1-Ar-14.inp')
+        cp2k = Cp2kInput(self._get_data_file('regtest-ot-1-Ar-14.inp'))
         self.assertEqual(True, cp2k.boolean(None, True))
         self.assertEqual(False, cp2k.boolean(None, False))
         self.assertEqual(True, cp2k.boolean('.T.', False))
@@ -28,19 +32,19 @@ class TestCp2kInput(unittest.TestCase):
         self.assertEqual(False, cp2k.boolean('F', True))
 
     def test_getpath(self):
-        cp2k = Cp2kInput('./data/regtest-ot-1-Ar-14.inp')
+        cp2k = Cp2kInput(self._get_data_file('regtest-ot-1-Ar-14.inp'))
         self.assertEqual('1.0E-10', cp2k.get_path('FORCE_EVAL / DFT / QS / EPS_DEFAULT'))
         fh = StringIO.StringIO(simple1)
         cp2k = Cp2kInput(filehandle=fh)
         self.assertRaises(ValueError, cp2k.get_path, '')
 
     def test_getpath_multiple(self):
-        cp2k = Cp2kInput('./data/regtest-ot-1-Ar-14-2.inp')
+        cp2k = Cp2kInput(self._get_data_file('regtest-ot-1-Ar-14-2.inp'))
         vals = cp2k.get_path('FORCE_EVAL / SUBSYS / COORD / *')
         self.assertEqual(['Ar 1 0 0', 'Ar 0 0 0'], vals)
 
     def test_getkeywordchecked(self):
-        cp2k = Cp2kInput('./data/regtest-ot-1-Ar-14-2.inp')
+        cp2k = Cp2kInput(self._get_data_file('regtest-ot-1-Ar-14-2.inp'))
         self.assertEqual(None, cp2k.get_keyword_checked('FORCE_EVAL / SUBSYS / DUMMY', float))
         self.assertEqual(['a'], cp2k.get_keyword_checked('FORCE_EVAL / SUBSYS / DUMMY', str))
         self.assertEqual(['a'], cp2k.get_keyword_checked('FORCE_EVAL / SUBSYS / DUMMY'))
@@ -50,13 +54,13 @@ class TestCp2kInput(unittest.TestCase):
         self.assertEqual(None, cp2k.get_keyword_checked(''))
 
     def test_cellvectors(self):
-        cp2k = Cp2kInput('./data/regtest-ot-1-Ar-14.inp')
+        cp2k = Cp2kInput(self._get_data_file('regtest-ot-1-Ar-14.inp'))
         a, b, c = cp2k.get_cell_vectors()
         self.assertTrue(np.allclose(a, np.array([4, 0, 0])))
         self.assertTrue(np.allclose(b, np.array([0, 5, 0])))
         self.assertTrue(np.allclose(c, np.array([0, 0, 6])))
 
-        cp2k = Cp2kInput('./data/regtest-ot-1-Ar-14-2.inp')
+        cp2k = Cp2kInput(self._get_data_file('regtest-ot-1-Ar-14.inp'))
         a, b, c = cp2k.get_cell_vectors()
         self.assertTrue(np.allclose(a, np.array([4, 0, 0])))
         self.assertTrue(np.allclose(b, np.array([0, 5, 0])))
