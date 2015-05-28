853f875d5afacc2ec11b5a4a78caa682e6c57281
Guido Falk von Rudorff <guido@vonrudorff.de>
2015-05-28 08:04:59 +0100

    added MDAnalysis and mdtraj to optional dependencies

---
+++ tests/test_DCD.py
@@ -0,0 +1,11 @@
+import unittest
+
+import euston.io as io
+
+class TestDCD(unittest.TestCase):
+    def _get_data_file(self, filename):
+        return os.path.join(os.path.dirname(os.path.realpath(__file__)), 'data', filename)
+
+    def test_create(self):
+	self.assertTrue(io.HAS_MDA)
+	self.assertTrue(io.HAS_MDT)
