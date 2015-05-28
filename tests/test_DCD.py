c6e0f38e5f4e6403339ed6859356247a71ded6a2
Guido Falk von Rudorff <guido@vonrudorff.de>
2015-05-28 10:41:57 +0100

    require compile for mdtraj

--- tests/test_DCD.py
+++ tests/test_DCD.py
@@ -2,7 +2,7 @@ import unittest
 
 import euston.io as io
 
-class TestDCD(unittest.TestCase):
+class Test_DCD(unittest.TestCase):
     def _get_data_file(self, filename):
         return os.path.join(os.path.dirname(os.path.realpath(__file__)), 'data', filename)
 
